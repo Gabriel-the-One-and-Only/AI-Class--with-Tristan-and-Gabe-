e5754ad58eb528aad09d791faedb69006d28f607
Gabriel-the-One-and-Only <Gabrieltechy@gmail.com>
2022-05-01 18:39:56 -0500

    Update P5_supplement.py

--- P5_MDP_RL/P5_supplement.py
+++ P5_MDP_RL/P5_supplement.py
@@ -18,6 +18,7 @@
 # Student Information #
 ######################
 ## Write your name and your partner name here
+#Gabriel Shultz and Tristan Jibben
 
 
 ######################
@@ -29,7 +30,7 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0
     return answerDiscount, answerNoise
 
 def question3a():
